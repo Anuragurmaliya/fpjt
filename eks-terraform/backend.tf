710e9baacf87ac742437dfd48f513851df6ba644
Anuragurmaliya <168172442+Anuragurmaliya@users.noreply.github.com>
2024-10-25 13:24:00 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "state-remote-store-backend-mongoproject/"
+    bucket = "mycloudcore008"
     key    = "eks/terraform.tfstate"
     region = "ap-south-1"
   }
