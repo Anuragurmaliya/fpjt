107685e9ee490a0c9fddfb6e38d242c8d1247bd4
Anurag <anuragurmaliya911@gmail.com>
2024-10-25 13:34:36 +0530

    anurag

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,14 +7,11 @@ terraform {
   }
 
   backend "s3" {
-<<<<<<< HEAD
-    bucket = "state-remote-store-backend-mongoproject"
-=======
     bucket = "mycloudcore008"
->>>>>>> 88f6d6910ec21c666261c75f10eac94a50beb893
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
 }
+  
