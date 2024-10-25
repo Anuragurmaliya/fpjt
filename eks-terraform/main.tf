baf3788401a0dc17f672ab116c43d580e0e5888b
Anurag <anuragurmaliya911@gmail.com>
2024-10-25 13:41:01 +0530

    anu

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
  #Creating IAM role for EKS
   resource "aws_iam_role" "master" {
-    name = "anurag-eks-master"
+    name = "mongodb-eks-master"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -36,7 +36,7 @@ provider "aws" {
   }
 
   resource "aws_iam_role" "worker" {
-    name = "anurag-eks-worker"
+    name = "mongodb-eks-worker"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -53,7 +53,7 @@ provider "aws" {
   }
 
   resource "aws_iam_policy" "autoscaler" {
-    name = "anurag-eks-autoscaler-policy"
+    name = "mongodb-eks-autoscaler-policy"
     policy = jsonencode({
       "Version": "2012-10-17",
       "Statement": [
